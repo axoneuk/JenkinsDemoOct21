de881b2c8b46912c3dbe41caf4602e1f442ffa9f
axoneuk <info@axone-tech.uk>
2021-10-16 06:32:31 +0100

    First commit

---
+++ src/test/java/uk/axone/devintest/maven/test/AppTest.java
@@ -0,0 +1,22 @@
+package uk.axone.devintest.maven.test;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+
+}
