cac00f6aca327ceb711e98d8d5288f2c52158269
axoneuk <info@axone-tech.uk>
2021-10-16 08:40:38 +0100

    Updated Unit tests (test 4 fails)

--- src/test/java/uk/axone/devintest/maven/test/AppTest.java
+++ src/test/java/uk/axone/devintest/maven/test/AppTest.java
@@ -1,5 +1,6 @@
 package uk.axone.devintest.maven.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
@@ -22,6 +23,7 @@ public class AppTest {
     @Test
     public void test4(){
         System.out.println("Running test 4");
+        Assert.fail();
     }
 
 }
